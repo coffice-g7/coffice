e97413099b8f01e308b0d605e7b89771c75eadfa
Antonio <aaon@cesar.school>
2024-04-21 18:19:35 -0300

    Push para sync

--- coffice/base/urls.py
+++ coffice/base/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
-    
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register_view, name='register'),
     path('room/', views.room, name="room"),
     
     
--- coffice/base/views.py
+++ coffice/base/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirecione para a página inicial após o registro
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
 
 def home(request):
     return render(request, 'home.html')
