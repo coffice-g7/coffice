c08fa90635a42c66d4c11ce1ebefab1c73091922
BernardoHeuer <bchg@cesar.school>
2024-06-11 18:15:59 -0300

    Testes dos filtros

---
+++ coffice/cypress/support/step-definitions/FiltrosCafeteria.cy.js
@@ -0,0 +1,67 @@
+// Funcionalidade: Filtrar cafeterias
+
+// Cenário 1:  Apenas um filtro aplicado na filtragem
+
+Given("que estou na página 'home'", () => {
+
+    cy.visit('http://127.0.0.1:8000/');
+    cy.wait(2000);
+});
+
+When("seleciono um filtro específico", () => {
+
+    cy.get('.third-line-filters').eq(1).within(() => {
+        cy.get('.div-filter-option-3').click();
+    });
+});
+
+And("clico em 'Gerar opções'", () => {
+
+    cy.get('.generate-options-button').click();
+});
+
+And("entro na página de detalhes da cafeteria", () => {
+
+    cy.get('.card').eq(0).click();
+});
+
+Then("devo ver na página da de detalhes da cafeteria o icone correspondente ao filtro escolhido", () => {
+
+    cy.get('#img-vegan').should('be.visible');
+});
+
+
+
+// Cenário 2: Dois filtros aplicados na filtrag
+
+Given("que estou na página 'home'", () => {
+
+    cy.visit('http://127.0.0.1:8000/');
+    cy.wait(2000);
+});
+
+When("seleciono dois filtros específicos", () => {
+
+    cy.get('.third-line-filters').eq(4).within(() => {
+        cy.get('.div-filter-option-1').click();
+    });
+    cy.get('.third-line-filters').eq(5).within(() => {
+        cy.get('.div-filter-option-2').click();
+    });
+});
+
+And("clico em 'Gerar opções'", () => {
+
+    cy.get('.generate-options-button').click();
+});
+
+And("entro na página de detalhes da cafeteria", () => {
+
+    cy.get('.card').eq(0).click();
+});
+
+Then("devo ver na página de detalhes da cafeteria os icones correspondentes aos filtros escolhidos", () => {
+
+    cy.get('#img-accessibility').should('be.visible');
+    cy.get('#img-parking').should('be.visible');;
+});
